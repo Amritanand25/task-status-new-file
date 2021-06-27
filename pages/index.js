6c008e716bf0b2f8431f4ef2a12976cc368285ba
Amritanand25 <48414060+Amritanand25@users.noreply.github.com>
2021-06-27 06:37:07 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Wrapper from '../components/taskComponents/Wrapper'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Wrapper />
+     
+    </div>
+  )
+}
